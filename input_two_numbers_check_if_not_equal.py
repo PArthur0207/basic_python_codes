51faa91e4ec4310eeaf09e3f31ced9b2570069df
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 23:01:36 +0800

    Ask the user for two numbers

--- input_two_numbers_check_if_not_equal.py
+++ input_two_numbers_check_if_not_equal.py
@@ -1,2 +1,4 @@
-# Ask the user to input two numbers 
+# Ask the user to input two numbers
+num1 = int(input("Input the first number here: "))
+num2 = int(input("Input the second number here: "))
 # Check if the inputted numbers is not equal, print NOT EQUAL if yes
