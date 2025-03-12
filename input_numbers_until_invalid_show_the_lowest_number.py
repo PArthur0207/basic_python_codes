3453e93d3617d120f555440e1617b2d1f17d6c81
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 16:05:57 +0800

    Put inputted numbers in a dictionary

--- input_numbers_until_invalid_show_the_lowest_number.py
+++ input_numbers_until_invalid_show_the_lowest_number.py
@@ -1,6 +1,8 @@
+# Put inputted numbers in a dictionary
+number_list = []
 # Asks user to input number
 number = int(input("Please input a number: "))
-# Put inputted numbers in a dictionary
+number_list.append(number)
 # Identify the lowest number
 # Repeat until input is invalid
 # Print the lowest number
