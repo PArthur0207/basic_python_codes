175b0aea11faa0f7c8f0d79a5456855c5cd62799
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 21:40:48 +0800

    Ask the user to input 10 numbers

--- sum_of_ten_numbers.py
+++ sum_of_ten_numbers.py
@@ -1,3 +1,6 @@
 # Ask the user to input ten numbers
+print("Please input 10 numbers: ")
+for rep in range(10):
+    input(f"Number {rep + 1}: ")
 # Add the result of the inputted numbers
 # Print the sum of the 10 numbers
