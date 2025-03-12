0cc2b5425aeaef386ad0752e02cb63a43bc28630
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 21:27:08 +0800

    Ask the user for the base and the exponent

--- first_number_raised_to_second_number.py
+++ first_number_raised_to_second_number.py
@@ -1,2 +1,4 @@
 # Ask the user for the base and the exponent
+base = int(input("Input the base here: "))
+exponent = int(input("Input the exponent here: "))
 # Print the result when the base is raised to the exponent
