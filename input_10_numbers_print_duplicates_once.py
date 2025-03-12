8ffbbde8b5241940e7ed6083c3a7dbc8f736892b
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 14:21:09 +0800

    check if number is already in dictionary and does not add to another dictionary if already existing

--- input_10_numbers_print_duplicates_once.py
+++ input_10_numbers_print_duplicates_once.py
@@ -1,12 +1,15 @@
 # Add the number on a dictionary
 numbers = []
+unique_numbers = []
 
 # Ask user to input 10 numbers
 print("Please enter 10 numbers: ")
 for rep in range(10):
     num = int(input(f"Number {rep + 1}: "))
     numbers.append(num)
-print(numbers)
+    if num not in unique_numbers:
+        unique_numbers.append(num)
+print(unique_numbers)
 
 # Check if the number is already inputted
 # Print all inputted numbers exactly once
