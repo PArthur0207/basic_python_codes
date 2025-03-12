d31939e8b2d2c9d13c36ae1d6a8e5486865884a3
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 22:38:26 +0800

    Ask the user to input two numbers

--- input_two_numbers_print_smaller_number.py
+++ input_two_numbers_print_smaller_number.py
@@ -1,2 +1,4 @@
 # Ask the user to input 2 numbers
+num1 = int(input("Input the first number here: "))
+num2 = int(input("Input the second number here: "))
 # Check which number is smaller, print the smaller one
