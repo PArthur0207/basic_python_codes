76775d30bc12b5c80c6a5194909b3fff0182ecdf
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 12:12:59 +0800

    Checks if the numbers have been inputted exactly ones and store the unque numbers in a variable

--- input_10_numbers_print_only_unique_numbers.py
+++ input_10_numbers_print_only_unique_numbers.py
@@ -1,7 +1,7 @@
 # Add each number in a list or dictionary.
 numbers = []
 number_counts = {}
-
+unique_numbers = []
 # Asks user to input 10 numbers.
 print("Please input 10 numbers: ")
 for rep in range (10):
@@ -12,5 +12,8 @@ print(number_counts)
 print(numbers)
 
 # Check if numbers have already been inputted.
+for num in numbers:
+    if number_counts[num] == 1:
+        unique_numbers.append(num)
+print(unique_numbers)
 # Print only the unique numbers.
-
