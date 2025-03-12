bf054300d19f91b05fd462059aa7ecb0b9f5bce6
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 20:22:40 +0800

    Print the sum of the two numbers

--- sum_of_two_numbers.py
+++ sum_of_two_numbers.py
@@ -2,3 +2,4 @@
 num1 = int(input("Input the first number here: "))
 num2 = int(input("Input the second number here: "))
 # Print the sum of the two numbers
+print(f"The sum of your two numbers is {num1 + num2}")
