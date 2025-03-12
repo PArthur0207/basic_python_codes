5998b060675f9bcdb6a10233118e6b51b8456143
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 21:30:27 +0800

    Print the result when the base is raised to the exponent

--- first_number_raised_to_second_number.py
+++ first_number_raised_to_second_number.py
@@ -2,3 +2,4 @@
 base = int(input("Input the base here: "))
 exponent = int(input("Input the exponent here: "))
 # Print the result when the base is raised to the exponent
+print(f"The base when raised to the exponent is {base ** exponent}")
