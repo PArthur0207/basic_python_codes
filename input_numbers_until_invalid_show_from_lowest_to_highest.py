1eefa6108bb0ee8527aa2ca16a5622e83099754d
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 16:59:17 +0800

    Sorts the numbers in ascending order, and prints the result)

--- input_numbers_until_invalid_show_from_lowest_to_highest.py
+++ input_numbers_until_invalid_show_from_lowest_to_highest.py
@@ -7,7 +7,9 @@ while True: # Keep asking until input is invalid
         number = int(input("Please input the number here: "))
         number_list.append(number)
     except ValueError:
-        print("Inalid input")
+        print("Invalid input")
         break
 
 # Print the numbers from lowest to highest
+number_list.sort()
+print(f"The number in ascending order are {number_list}")
