a391575ea0b53e8f98cb2ea802456d754f18f7e3
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 23:27:56 +0800

    Asks te user to input two numbers

--- remainder_when_two_numbers_is_divided.py
+++ remainder_when_two_numbers_is_divided.py
@@ -1,2 +1,4 @@
 # Ask the user to input 2 numbers
+num1 = int(input("Input the first number here: "))
+num2 = int(input("Input the second number here: "))
 # Print the remainder when the numbers are divided
