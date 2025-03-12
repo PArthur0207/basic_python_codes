d722d7a869cd3d2e7addb4b2dc6bade0fdd3f118
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 21:17:11 +0800

    Print the decimal of the two numbers with the decimals

--- quotient_of_two_numbers_with_decimal.py
+++ quotient_of_two_numbers_with_decimal.py
@@ -1,4 +1,5 @@
 # Ask the user to input two numbers
-num1 = int(input("Input the first number here: "))
-num2 = int(input("Input the second number here: "))
+num1 = float(input("Input the first number here: "))
+num2 = float(input("Input the second number here: "))
 # Print the quotiet of two numbers with decimal
+print(f"The quotient of your two numbers with decimals is {num1 / num2}")
