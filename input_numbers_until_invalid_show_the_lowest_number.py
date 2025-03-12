1eceb025834c08bd588b62a6236eb978d6c45f80
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 16:14:57 +0800

    Identifying the lowest number on the list

--- input_numbers_until_invalid_show_the_lowest_number.py
+++ input_numbers_until_invalid_show_the_lowest_number.py
@@ -4,5 +4,6 @@ number_list = []
 number = int(input("Please input a number: "))
 number_list.append(number)
 # Identify the lowest number
+lowest_number = min(number_list)
 # Repeat until input is invalid
 # Print the lowest number
