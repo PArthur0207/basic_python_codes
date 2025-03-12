4530861772f41f62e0e05668394533bef4f59181
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 15:08:26 +0800

    Adds the inputted numbers in a dictionary

--- input_number_until_invalid_show_unique_or_duplicate.py
+++ input_number_until_invalid_show_unique_or_duplicate.py
@@ -1,5 +1,10 @@
+# Put numbers into a dictionary
+unique_numbers = []
+
 # Asks user to input numbers
 number = int(input("Please enter a number: "))
-# Put numbers into a dictionary
+unique_numbers.append(number)
+print(unique_numbers)
+
 # Check if number is in dictionary print 'duplicate' if not print 'unique'
 # Repeat process until the input is invalid
