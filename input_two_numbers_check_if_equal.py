3fe0ba6490bcd5c5a1422dd1c79fc56bd692d590
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 20:14:32 +0800

    Check if numbers are equal, print EQUAL if yes, NOT EQUAL if not

--- input_two_numbers_check_if_equal.py
+++ input_two_numbers_check_if_equal.py
@@ -2,3 +2,7 @@
 num1 = int(input("Input the first number here: "))
 num2 = int(input("Input the second number here: "))
 # Check if numbers are equal, print EQUAL if yes, NOT EQUAL if not
+if num1 == num2:
+    print("EQUAL")
+else:
+    print("NOT EQUAL")
