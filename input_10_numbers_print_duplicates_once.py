322afb39c4466a956bcd986505ae0248d2d7347f
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 14:28:35 +0800

    Print all the numbers inputted exactly once, and cleans the code

--- input_10_numbers_print_duplicates_once.py
+++ input_10_numbers_print_duplicates_once.py
@@ -7,9 +7,8 @@ print("Please enter 10 numbers: ")
 for rep in range(10):
     num = int(input(f"Number {rep + 1}: "))
     numbers.append(num)
-    if num not in unique_numbers:
+    if num not in unique_numbers: # Check if the number is already inputted
         unique_numbers.append(num)
-print(unique_numbers)
 
-# Check if the number is already inputted
 # Print all inputted numbers exactly once
+print(f"The numbers you inputted (without duplicates) are :{unique_numbers}")
