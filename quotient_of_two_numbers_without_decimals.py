4d0752f6d7b976054bbaf6ea6b51b61f8e92a33d
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 23:24:35 +0800

    Print the quotient of the two numbers without the decimal

--- quotient_of_two_numbers_without_decimals.py
+++ quotient_of_two_numbers_without_decimals.py
@@ -1,4 +1,5 @@
 # Ask the user to input two numbers
-num1 = float(input("Input the first number here: "))
-num2 = float(input("Input the second number here: "))
+num1 = int(input("Input the first number here: "))
+num2 = int(input("Input the second number here: "))
 # Print the sum of the two numbers without the decimals
+print(f"The quotient of your two numbers without the decimals is {int(num1 / num2)} ")
