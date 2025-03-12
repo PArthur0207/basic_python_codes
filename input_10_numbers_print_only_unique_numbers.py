bd9de02bb6d682d8a61861f2e65643617dc73b21
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 12:16:29 +0800

    Print the unique numbers and clean the code

--- input_10_numbers_print_only_unique_numbers.py
+++ input_10_numbers_print_only_unique_numbers.py
@@ -2,18 +2,18 @@
 numbers = []
 number_counts = {}
 unique_numbers = []
+
 # Asks user to input 10 numbers.
 print("Please input 10 numbers: ")
 for rep in range (10):
     num = int(input(f"Number {rep + 1}: "))
     numbers.append(num)
     number_counts[num] = number_counts.get(num, 0) + 1
-print(number_counts)
-print(numbers)
 
 # Check if numbers have already been inputted.
 for num in numbers:
     if number_counts[num] == 1:
         unique_numbers.append(num)
-print(unique_numbers)
+
 # Print only the unique numbers.
+print(f"Numbers without duplicate {unique_numbers}")
