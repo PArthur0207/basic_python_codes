5e5f35ccf8055d7d504dc2b8938451b74c1aa851
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 23:30:50 +0800

    Print the remainder when two numbers are divided using modulo

--- remainder_when_two_numbers_is_divided.py
+++ remainder_when_two_numbers_is_divided.py
@@ -2,3 +2,4 @@
 num1 = int(input("Input the first number here: "))
 num2 = int(input("Input the second number here: "))
 # Print the remainder when the numbers are divided
+print(f"The remainder when your two numbers are divided is {num1 % num2}")
