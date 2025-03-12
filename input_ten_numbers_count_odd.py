248346384ad8e9e3a1879751d582b7bcb0d6a833
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 22:11:35 +0800

    Print how many are odd numbers

--- input_ten_numbers_count_odd.py
+++ input_ten_numbers_count_odd.py
@@ -1,10 +1,12 @@
 # Intialized for proper increment
 odd_count = 0
+
 # Asks the user to input ten numbers
 print("Please input 10 numbers: ")
 for rep in range(10):
-    number = input(f"Number {rep + 1}") # Check if odd or not and count how many odd
+    number = int(input(f"Number {rep + 1}: ")) # Check if odd or not and count how many odd
     if number % 2 == 1:
         odd_count += 1
 
 # Print the count of how many odd numbers
+print(f"You have inputed {odd_count} odd numbers")
