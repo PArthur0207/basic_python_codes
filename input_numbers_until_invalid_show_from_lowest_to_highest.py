d3e0b80cd5c8af91e2ba1fa3f289f444af474e94
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 16:39:00 +0800

    Asks user for numbers

--- input_numbers_until_invalid_show_from_lowest_to_highest.py
+++ input_numbers_until_invalid_show_from_lowest_to_highest.py
@@ -1,4 +1,5 @@
 # Asks the user to input number
+number = int(input("Please input the number here: "))
 # Store the inputted numbers in a dictionary
 # Keep asking until input is invalid
 # Print the numbers from lowes to highest
