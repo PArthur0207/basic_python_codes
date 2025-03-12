6a80ced32e2a140631bac1e7511031004dd73576
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 19:49:12 +0800

    Check which number is bigger and prints the bigger number

--- input_two_numbers_show_the_bigger_number.py
+++ input_two_numbers_show_the_bigger_number.py
@@ -2,3 +2,9 @@
 num1 = int(input("Input the first number here: "))
 num2 = int(input("Input the second number here: "))
 # Print the bigger number
+if num1 > num2:
+    print(f"The bigger number is {num1}")
+elif num2 > num1:
+    print(f"The bigger number is {num2}")
+else:
+    print("There is no bigger number")
