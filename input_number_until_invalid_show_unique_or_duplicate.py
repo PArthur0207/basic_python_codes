f2fe59b127706ccbb7eae75e1cfa4815454aa196
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 15:33:25 +0800

    Repeat the code until the user inputted an invalid input and clean the code

--- input_number_until_invalid_show_unique_or_duplicate.py
+++ input_number_until_invalid_show_unique_or_duplicate.py
@@ -1,13 +1,15 @@
 # Put numbers into a dictionary
 unique_numbers = []
 
-# Asks user to input numbers
-number = int(input("Please enter a number: "))
-if number in unique_numbers:  # Check if number is in dictionary print 'duplicate' if not print 'unique'
-    print("Duplicate")
-else:
-    print("Unique")
-    unique_numbers.append(number)
-print(unique_numbers)
-
-# Repeat process until the input is invalid
+while True: # Repeat process until the input is invalid
+    try:
+        # Asks user to input numbers
+        number = int(input("Please enter a number: "))
+        if number in unique_numbers:  # Check if number is in dictionary print 'duplicate' if not print 'unique'
+            print("Duplicate")
+        else:
+            print("Unique")
+            unique_numbers.append(number)
+    except ValueError:
+        print("Ivalid input, ending run")    
+        break
