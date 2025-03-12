2829caf8d9b2df58c44aada4af56d0fd6522121a
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 22:25:13 +0800

    Loops 101 times, check if divisible by 10, print if yes

--- print_0_to_100_except_ending_in_zero.py
+++ print_0_to_100_except_ending_in_zero.py
@@ -1,3 +1,4 @@
-# Make aloop that repeats 101 times
-# Check if number is divisible by 10
-# Print if not divisible by 10
+# Make a loop that repeats 101 times
+for num in range(101):
+    if num % 10 != 0: # Check if number is divisible by 10
+        print(num) # Print if not divisible by 10
