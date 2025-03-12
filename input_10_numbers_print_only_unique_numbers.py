d077366246c696196c1e4cc7fd74bf0fac9c91e5
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 12:00:20 +0800

    Made the inputs an integer, and put the inputted numbers in a dictionary

--- input_10_numbers_print_only_unique_numbers.py
+++ input_10_numbers_print_only_unique_numbers.py
@@ -1,9 +1,13 @@
+# Add each number in a list or dictionary.
+numbers = []
+
 # Asks user to input 10 numbers.
 print("Please input 10 numbers: ")
 for rep in range (10):
-    num = input(f"Number {rep + 1}: ")
-    print(num)
-# Add each number in a list or dictionary.
+    num = int(input(f"Number {rep + 1}: "))
+    numbers.append(num)
+print(numbers)
+
 # Check if numbers have already been inputted.
 # Print only the unique numbers.
 
