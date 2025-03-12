93de367038545bdc0cbc3780a49380b7a14c67ef
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 23:21:43 +0800

    Asks the user to input two numbers

--- quotient_of_two_numbers_without_decimals.py
+++ quotient_of_two_numbers_without_decimals.py
@@ -1,2 +1,4 @@
 # Ask the user to input two numbers
+num1 = float(input("Input the first number here: "))
+num2 = float(input("Input the second number here: "))
 # Print the sum of the two numbers without the decimals
