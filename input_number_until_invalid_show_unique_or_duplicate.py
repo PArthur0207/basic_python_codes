6f455cdf241a87d85295f4b336756d69e3cd7726
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 15:21:31 +0800

    Checks if number is in dictionary, print duplicate if yes, print unique if not

--- input_number_until_invalid_show_unique_or_duplicate.py
+++ input_number_until_invalid_show_unique_or_duplicate.py
@@ -3,8 +3,11 @@ unique_numbers = []
 
 # Asks user to input numbers
 number = int(input("Please enter a number: "))
-unique_numbers.append(number)
+if number in unique_numbers:  # Check if number is in dictionary print 'duplicate' if not print 'unique'
+    print("Duplicate")
+else:
+    print("Unique")
+    unique_numbers.append(number)
 print(unique_numbers)
 
-# Check if number is in dictionary print 'duplicate' if not print 'unique'
 # Repeat process until the input is invalid
