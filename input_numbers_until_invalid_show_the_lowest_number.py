15584ccf7ec0954bd2c195d4ac1b6d085dba6c13
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 16:20:26 +0800

    Keep asking for number until input is invalid

--- input_numbers_until_invalid_show_the_lowest_number.py
+++ input_numbers_until_invalid_show_the_lowest_number.py
@@ -1,9 +1,14 @@
 # Put inputted numbers in a dictionary
 number_list = []
-# Asks user to input number
-number = int(input("Please input a number: "))
-number_list.append(number)
+
+while True: # Repeat until input is invalid
+    try:
+        # Asks user to input number
+        number = int(input("Please input a number: "))
+        number_list.append(number)
+    except ValueError:
+        print("Invalid input")
+        break
 # Identify the lowest number
 lowest_number = min(number_list)
-# Repeat until input is invalid
 # Print the lowest number
