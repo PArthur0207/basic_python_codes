4985fdd4ad6f48713659ac8fcd44d617f2cae906
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 14:49:17 +0800

    Pseudocode

---
+++ input_number_until_invalid_show_unique_or_duplicate.py
@@ -0,0 +1,4 @@
+# Asks user to input numbers
+# Put numbers into a dictionary
+# Check if number is in dictionary print 'duplicate' if not print 'unique'
+# Repeat process until the input is invalid
