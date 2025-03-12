ffdd99136fc6e1417c041ec8cf45f865d3a27652
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 11:54:47 +0800

    Asks the user for 10 numbers

--- input_10_numbers_print_only_unique_numbers.py
+++ input_10_numbers_print_only_unique_numbers.py
@@ -1,4 +1,8 @@
 # Asks user to input 10 numbers.
+print("Please input 10 numbers: ")
+for rep in range (10):
+    num = input(f"Number {rep + 1}: ")
+    print(num)
 # Add each number in a list or dictionary.
 # Check if numbers have already been inputted.
 # Print only the unique numbers.
