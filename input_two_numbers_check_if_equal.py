93a52dbf47b10c3a13f385c8ba51713ddefeab79
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 19:53:59 +0800

    Asks the user to input two numbers

--- input_two_numbers_check_if_equal.py
+++ input_two_numbers_check_if_equal.py
@@ -1,2 +1,4 @@
 # Asks the user to input two numbers
+num1 = int(input("Input the first number here: "))
+num2 = int(input("Input the second number here: "))
 # Check if numbers are equal, print EQUAL if yes, NOT EQUAL if not
