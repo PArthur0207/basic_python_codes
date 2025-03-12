5bd54ad2c67a1a881b23b0395f520446f6c5a5a8
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 12:06:16 +0800

    Counts how many times each integer have been inputted

--- input_10_numbers_print_only_unique_numbers.py
+++ input_10_numbers_print_only_unique_numbers.py
@@ -1,11 +1,14 @@
 # Add each number in a list or dictionary.
 numbers = []
+number_counts = {}
 
 # Asks user to input 10 numbers.
 print("Please input 10 numbers: ")
 for rep in range (10):
     num = int(input(f"Number {rep + 1}: "))
     numbers.append(num)
+    number_counts[num] = number_counts.get(num, 0) + 1
+print(number_counts)
 print(numbers)
 
 # Check if numbers have already been inputted.
