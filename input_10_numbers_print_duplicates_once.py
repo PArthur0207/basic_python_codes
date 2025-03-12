921538e7084db1466e704b384832344c046040a3
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-12 13:48:45 +0800

    Adds the inputted numbers in a dictionary

--- input_10_numbers_print_duplicates_once.py
+++ input_10_numbers_print_duplicates_once.py
@@ -1,8 +1,12 @@
+# Add the number on a dictionary
+numbers = []
+
 # Ask user to input 10 numbers
 print("Please enter 10 numbers: ")
 for rep in range(10):
     num = int(input(f"Number {rep + 1}: "))
-    print(num)
-# Add the number on a dictionary
+    numbers.append(num)
+print(numbers)
+
 # Check if the number is already inputted
 # Print all inputted numbers exactly once
