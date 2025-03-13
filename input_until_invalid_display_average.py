25d78ac8f6c17e625fa53897a47e451e99bc8e1b
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 13:13:17 +0800

    Keep asking until input is invalid, count every loop

--- input_until_invalid_display_average.py
+++ input_until_invalid_display_average.py
@@ -1,4 +1,12 @@
-# Ask the user to input a number
-number = float(input("Input your number here: "))
-# Keep asking until user input is invalid, count every loop
+count = 0 # Initialized for counting the loops
+
+while True: # Keep asking until user input is invalid, count every loop
+    try:
+        # Ask the user to input a number
+        number = float(input("Input your number here: "))
+        count += 1
+    except ValueError:
+        print("Invalid input. Ending run")
+        break
+
 # Divide the total of the number by the number of times looped and show the result  
