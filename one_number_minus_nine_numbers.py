7e81e9aa3146a2e8d139ce5b4e89937cab42e5a1
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 10:43:05 +0800

    Continuosly substract from the first number, and printed the result

--- one_number_minus_nine_numbers.py
+++ one_number_minus_nine_numbers.py
@@ -2,7 +2,9 @@
 number1 = int(input("Please enter the first number here: "))
 
 # Ask the user to input the 9 remaining number
-for rep in range(10):
+for rep in range(9):
     num2_10 = int(input("Please enter the next number here: "))
-# Continually subtract the succeeding 9 numbers from the first number
+    number1 -= num2_10 # Continually subtract the succeeding 9 numbers from the first number
+
 # Print the result
+print(f"Your first number minus the rest of your numbers is {number1}")
