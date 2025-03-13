a9177c33d49a2500de3a9c5bf8c9f361b99523f9
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 10:52:18 +0800

    Check if even or not, add to count if yes, and printed the result

--- input_ten_numbers_count_even.py
+++ input_ten_numbers_count_even.py
@@ -1,6 +1,11 @@
+# Initialize count for incrementing
+even_count = 0
 # Ask the user to input 10 numbers
 print("Please input 10 numbers: ")
 for rep in range(10):
     number =  int(input(f"Input number {rep + 1} here: "))
-# Check if even or not, add to cout if even
+    if number % 2 == 0: # Check if even or not, add to count if even
+        even_count += 1
+
 # Print the result
+print(f"You have inputted {even_count} even numbers")
