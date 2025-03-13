6b42e25b541689db61c570998d428cb769a4b868
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 13:51:19 +0800

    Keep asking until user input is invalid

--- input_until_invalid_display_the_number_with_most_duplicate.py
+++ input_until_invalid_display_the_number_with_most_duplicate.py
@@ -1,6 +1,12 @@
-# Ask the user to input number
-number = int(input("Please input your number her: "))
 # Keep asking until user input is invalid
+while True:
+    try:
+        # Ask the user to input number
+        number = int(input("Please input your number here: "))
+    except ValueError:
+        print("Invalid input. Ending run")
+        break
+
 # Put inputted numbers into a list
 # Count how many time each number is inputted
 # Check which number is most frequent and print the result       
