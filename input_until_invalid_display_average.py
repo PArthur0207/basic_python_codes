8c592144dd7bdbde5a999406cb3dec74aa807fb9
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 13:10:45 +0800

    Asks the user to input a number

--- input_until_invalid_display_average.py
+++ input_until_invalid_display_average.py
@@ -1,3 +1,4 @@
 # Ask the user to input a number
+number = float(input("Input your number here: "))
 # Keep asking until user input is invalid, count every loop
-# Divide the total of the number by the number of times looped and show the result
+# Divide the total of the number by the number of times looped and show the result  
