f36d42b70940f0beaa0c0bb657fb4b7c9f3c9116
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 11:34:29 +0800

    Pseudocode

---
+++ input_ten_numbers_print_duplicates_only.py
@@ -0,0 +1,4 @@
+# Ask the user to input 10 numbers
+# Put inputted numbers into a dictionary
+# Check if inputed number is already in the dictionary, put into a different dictionary (only once) if yes
+# Print the duplicated numbers only once
