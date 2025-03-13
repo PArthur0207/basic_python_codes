ed93061b7ef55daff213a2a6182d78fdc22a0a46
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 10:48:22 +0800

    Ask the user to input 10 numbers

--- input_ten_numbers_count_even.py
+++ input_ten_numbers_count_even.py
@@ -1,3 +1,6 @@
 # Ask the user to input 10 numbers
+print("Please input 10 numbers: ")
+for rep in range(10):
+    number =  int(input(f"Input number {rep + 1} here: "))
 # Check if even or not, add to cout if even
 # Print the result
