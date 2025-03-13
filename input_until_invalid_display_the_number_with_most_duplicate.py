de253afe04111b5c8fc79da29cb861f2424cd8e9
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 13:49:38 +0800

    Asks the user to input a number

--- input_until_invalid_display_the_number_with_most_duplicate.py
+++ input_until_invalid_display_the_number_with_most_duplicate.py
@@ -1,4 +1,5 @@
 # Ask the user to input number
+number = int(input("Please input your number her: "))
 # Keep asking until user input is invalid
 # Put inputted numbers into a list
 # Count how many time each number is inputted
