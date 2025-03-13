ac8b077f2b3e6aca5a62daace83dde9379955f6b
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 13:01:02 +0800

    Keeps asking until user input is invalid

--- input_numbers_until_invalid_show_from_highest_to_lowest.py
+++ input_numbers_until_invalid_show_from_highest_to_lowest.py
@@ -1,5 +1,11 @@
-# Ask the user to input a number
-number = int(input("Please input your number here: "))
 # Keep asking until the user input is invalid
+while True:
+    try:
+        # Ask the user to input a number
+        number = int(input("Please input your number here: "))
+    except ValueError:
+        print("Invalid input. Ending run")
+        break
+
 # Add the inputted numbers in a dictionary
 # Put into highest to lowest order, and print the result
