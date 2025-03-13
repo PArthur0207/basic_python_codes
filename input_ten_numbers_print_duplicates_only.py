81dabda4bdc9a69ddee457e76aefe5843b286f6c
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 11:36:53 +0800

    Asks the user to input ten numbers

--- input_ten_numbers_print_duplicates_only.py
+++ input_ten_numbers_print_duplicates_only.py
@@ -1,4 +1,6 @@
 # Ask the user to input 10 numbers
+for rep in range(10):
+    number = int(input(f"Please enter the number {rep + 1} here: "))
 # Put inputted numbers into a dictionary
 # Check if inputed number is already in the dictionary, put into a different dictionary (only once) if yes
 # Print the duplicated numbers only once
