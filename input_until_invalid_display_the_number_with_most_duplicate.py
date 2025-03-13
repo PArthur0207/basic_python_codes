2534f9f454a5369b6593600b7697581a27a20ebc
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 13:48:48 +0800

    Pseudocode

---
+++ input_until_invalid_display_the_number_with_most_duplicate.py
@@ -0,0 +1,5 @@
+# Ask the user to input number
+# Keep asking until user input is invalid
+# Put inputted numbers into a list
+# Count how many time each number is inputted
+# Check which number is most frequent and print the result       
