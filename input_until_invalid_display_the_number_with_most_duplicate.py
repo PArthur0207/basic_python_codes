5c7b1aa98f32d9287fbea2554856c8b0e8b9709a
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 13:52:48 +0800

    Put inputted numbers on a list

--- input_until_invalid_display_the_number_with_most_duplicate.py
+++ input_until_invalid_display_the_number_with_most_duplicate.py
@@ -1,12 +1,14 @@
+# Put inputted numbers into a list
+number_list = []
 # Keep asking until user input is invalid
 while True:
     try:
         # Ask the user to input number
         number = int(input("Please input your number here: "))
+        number_list.append(number)
     except ValueError:
         print("Invalid input. Ending run")
         break
 
-# Put inputted numbers into a list
 # Count how many time each number is inputted
 # Check which number is most frequent and print the result       
