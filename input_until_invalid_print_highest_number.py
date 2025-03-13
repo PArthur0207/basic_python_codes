f00cc05a7904266f23e45dcbbc6465100c29319c
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 12:11:49 +0800

    Asks the user to input a number

--- input_until_invalid_print_highest_number.py
+++ input_until_invalid_print_highest_number.py
@@ -1,4 +1,5 @@
 # Ask the user to input a number
+number = int(input("Enter your number here: "))
 # Keep asking until the user becomes invalid
 # Compare numbers to see which is highest in each iteration
 # Print the highest number
