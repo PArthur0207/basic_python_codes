c91d880323786bd936ff5cc54e88cabf967a86c2
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 12:18:41 +0800

    Compare numbers every iteration to find the biggest number, and prints the biggest number when loop has ended

--- input_until_invalid_print_highest_number.py
+++ input_until_invalid_print_highest_number.py
@@ -1,11 +1,17 @@
+# Initialize current highest number
+current_highest_number = None
+
 # Keep asking until the user becomes invalid
 while True:
     try:    
         # Ask the user to input a number
         number = int(input("Enter your number here: "))
+        # Compare numbers to see which is highest in each iteration
+        if current_highest_number is None or number > current_highest_number:
+            current_highest_number = number
     except ValueError:
         print("Invalid input, ending run.")
         break    
 
-# Compare numbers to see which is highest in each iteration
 # Print the highest number
+print(f"The highest number you inputted is {current_highest_number}")
