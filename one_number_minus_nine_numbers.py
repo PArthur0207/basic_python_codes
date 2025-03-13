47b521f914e94f0ef5d31ea528abd969f8f07017
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 10:39:22 +0800

    Asks the user to input the 9 remaining numbers

--- one_number_minus_nine_numbers.py
+++ one_number_minus_nine_numbers.py
@@ -1,5 +1,8 @@
 # Ask the user to input one number
 number1 = int(input("Please enter the first number here: "))
+
 # Ask the user to input the 9 remaining number
+for rep in range(10):
+    num2_10 = int(input("Please enter the next number here: "))
 # Continually subtract the succeeding 9 numbers from the first number
 # Print the result
