e887d5add46051d21b0e2f66ccaaf47418a2204b
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 11:46:04 +0800

    Check if number is in dictionary, put in seperate dictionary (only ones) if yes, and print the duplicated numbers only once

--- input_ten_numbers_print_duplicates_only.py
+++ input_ten_numbers_print_duplicates_only.py
@@ -1,11 +1,17 @@
 # Put inputted numbers into a dictionary
 number_list = []
+duplicates = []
 
 # Ask the user to input 10 numbers
 for rep in range(10):
     number = int(input(f"Please enter the number {rep + 1} here: "))
+    if number in number_list:
+        # Check if inputed number is already in the dictionary, put into a different dictionary (only once) if yes
+        if number in duplicates: 
+            pass
+        else:
+            duplicates.append(number)
     number_list.append(number)
-print(number_list)
 
-# Check if inputed number is already in the dictionary, put into a different dictionary (only once) if yes
 # Print the duplicated numbers only once
+print(duplicates)
