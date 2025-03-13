7e4ec11cf392364bf233593cb7e6b091bdea88b5
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 11:13:48 +0800

    Print the numbers in between if the second number is smaller

--- all_numbers_between_two_numbers.py
+++ all_numbers_between_two_numbers.py
@@ -1,8 +1,13 @@
 # Ask the user to input 2 numbers
 num1 = int(input("Input the first number here: "))
 num2 = int(input("Input the second number here: "))
+
 # Print the numbers in between if the first number is smaller
 if num1 < num2:
     for number in range(num1 + 1,num2):
         print(number)
+
 # Print the numbers in between if the second number is smaller
+elif num2 < num1:
+    for number in range(num1 - 1, num2, -1):
+        print(number)
