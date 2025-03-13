8c23fa2469474ecde4f4249a18520e74b4cbc4ca
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 13:16:41 +0800

    Divide the total of the inputted values by the number of loops, and print the result

--- input_until_invalid_display_average.py
+++ input_until_invalid_display_average.py
@@ -1,12 +1,15 @@
 count = 0 # Initialized for counting the loops
+total = 0 # Initialized to get the total of every number inputted
 
 while True: # Keep asking until user input is invalid, count every loop
     try:
         # Ask the user to input a number
         number = float(input("Input your number here: "))
         count += 1
+        total += number
     except ValueError:
         print("Invalid input. Ending run")
         break
 
 # Divide the total of the number by the number of times looped and show the result  
+print(f"The average of what you inputted is {total / count}")
