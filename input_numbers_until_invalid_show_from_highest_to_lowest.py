0dcad1e1661330f42827b59b8da674e000b521c9
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 13:04:34 +0800

    Added the inputted numbers into a dictionary

--- input_numbers_until_invalid_show_from_highest_to_lowest.py
+++ input_numbers_until_invalid_show_from_highest_to_lowest.py
@@ -1,11 +1,15 @@
+# Add the inputted numbers in a dictionary
+number_list = []
+
 # Keep asking until the user input is invalid
 while True:
     try:
         # Ask the user to input a number
         number = int(input("Please input your number here: "))
+        number_list.append(number)
     except ValueError:
         print("Invalid input. Ending run")
         break
+print(number_list)
 
-# Add the inputted numbers in a dictionary
 # Put into highest to lowest order, and print the result
