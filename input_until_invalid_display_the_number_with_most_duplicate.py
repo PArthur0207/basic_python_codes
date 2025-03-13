e78a776cc15b77ceca35f889597fcc47e0916cb8
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-13 13:58:50 +0800

    Checks which number have been inputted the most and prints the result

--- input_until_invalid_display_the_number_with_most_duplicate.py
+++ input_until_invalid_display_the_number_with_most_duplicate.py
@@ -17,5 +17,14 @@ while True:
     except ValueError:
         print("Invalid input. Ending run")
         break
+   
+most_frequent = None # Placeholder for the most frequent number inputted
+most_frequent_count = 0   # Initialized to count how many times the most frequent number is inputted
 
-# Check which number is most frequent and print the result       
+# Check which number is most frequent and print the result 
+for num, count in duplicate_count.items():
+    if count > most_frequent_count:
+        most_frequent = num
+        most_frequent_count = count
+
+print(f"Most frequent number is {most_frequent} inputtes {most_frequent_count} times")
